f682dc5030ea2bb5e71299c50f0952fb62ae3480
Nommiiee <kunalkashyp3@gmail.com>
2023-08-13 21:16:58 +0530

    reworked pages, added assets, new pages, new path, CSS rebuild, Image Counter

--- data/JS-Scripts/main.js
+++ data/JS-Scripts/main.js
@@ -1,12 +1,12 @@
 const navigationPanel = `<div class="" id="nav">
-<nav class="w-full h-screen absolute md:sticky top-0 bg-slate-900 -translate-x-full md:-translate-x-0 transtition-all duration-500  md:h-min"
+<nav class="w-full h-screen absolute top-0 bg-slate-900 -translate-x-full  transtition-all duration-500 lg:sticky lg:-translate-x-0 lg:h-min"
     id="navigation">
-    <div class=" flex flex-col md:flex-row text-white w-full">
+    <div class=" flex flex-col lg:flex-row  text-white w-full">
         <button
-            class="w-full text-center border-2 border-white text-lg font-medium hover:bg-black transtition-all duration-300 text-white p-2 menu-btn md:hidden">
+            class="w-full text-center border-2 border-white text-lg font-medium hover:bg-black transtition-all duration-300 text-white p-2 menu-btn lg:hidden">
             Close
         </button>
-        <a class="p-4 border-b-2 border-white w-full flex item-center gap-2 hover:bg-amber-500 hover:text-black hover:border-amber-500 font-medium transtition-all duration-500 md:justify-center"
+        <a class="p-4 border-b-2 border-white w-full flex item-center gap-2 hover:bg-amber-500 hover:text-black hover:border-amber-500 font-medium transtition-all duration-500 lg:justify-center"
             href="../index.html">
             <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
@@ -14,11 +14,22 @@ const navigationPanel = `<div class="" id="nav">
                         d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg>
             </span>
-            <span>Homepage</span>
+            <span>VRChive</span>
         </a>
-        <a class="p-4 border-b-2 border-white w-full flex item-center gap-2 hover:bg-amber-500 hover:text-black hover:border-amber-500 font-medium transtition-all duration-500 md:justify-center"
+        <a class="p-4 border-b-2 border-white w-full flex item-center gap-2 hover:bg-amber-500 hover:text-black hover:border-amber-500 font-medium transtition-all duration-500 lg:justify-center"
+            href="./Assets.html">
+            <span class="lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
+                </svg>
+            </span>
+            <span>Assets</span>
+        </a>
+        <a class="p-4 border-b-2 border-white w-full flex item-center gap-2 hover:bg-amber-500 hover:text-black hover:border-amber-500 font-medium transtition-all duration-500 lg:justify-center"
             href="./Avatars.html">
-            <span class="md:hidden">
+            <span class="lg:hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
@@ -27,20 +38,9 @@ const navigationPanel = `<div class="" id="nav">
             </span>
             <span>Avatar</span>
         </a>
-        <a class="p-4 border-b-2 border-white w-full flex item-center gap-2 hover:bg-amber-500 hover:text-black hover:border-amber-500 font-medium transtition-all duration-500 md:justify-center"
-            href="./Avatar_Original.html">
-            <span class="md:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
-                </svg>
-            </span>
-            <span>Avatar Original</span>
-        </a>
-        <a class="p-4 border-b-2 border-white w-full flex item-center gap-2 hover:bg-amber-500 hover:text-black hover:border-amber-500 font-medium transtition-all duration-500 md:justify-center"
+        <a class="p-4 border-b-2 border-white w-full flex item-center gap-2 hover:bg-amber-500 hover:text-black hover:border-amber-500 font-medium transtition-all duration-500 lg:justify-center"
             href="./clothes.html">
-            <span class="md:hidden">
+            <span class="lg:hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
@@ -49,32 +49,40 @@ const navigationPanel = `<div class="" id="nav">
             </span>
             <span>Clothing</span>
         </a>
-        <a class="p-4 border-b-2 border-white w-full flex item-center gap-2 hover:bg-amber-500 hover:text-black hover:border-amber-500 font-medium transtition-all duration-500 md:justify-center"
-            href="./Clothes_Original.html">
-            <span class="md:hidden">
+        <a class="p-4 border-b-2 border-white w-full flex item-center gap-2 hover:bg-amber-500 hover:text-black hover:border-amber-500 font-medium transtition-all duration-500 lg:justify-center"
+            href="./shaders.html">
+            <span class="lg:hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
                 </svg>
             </span>
-            <span>Clothing Original</span>
+            <span>Shaders</span>
+        </a>
+        <a class="p-4 border-b-2 border-white w-full flex item-center gap-2 hover:bg-amber-500 hover:text-black hover:border-amber-500 font-medium transtition-all duration-500 lg:justify-center"
+            href="./textures.html">
+            <span class="lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
+                </svg>
+            </span>
+            <span>Textures</span>
         </a>
     </div>
 </nav>
 <button
-    class="w-full border-2 border-white text-lg font-medium hover:bg-black transtition-all duration-300 text-white p-2 menu-btn md:hidden">
+    class="w-full border-2 border-white text-lg font-medium hover:bg-black transtition-all duration-300 text-white p-2 menu-btn lg:hidden">
     Menu
 </button>
 </div>`;
 
-const navContainer = document.getElementById("nav-container");
-navContainer.innerHTML = navigationPanel;
+    const navContainer = document.getElementById("nav-container");
+    navContainer.innerHTML = navigationPanel;
 
-if (
-  document.title == "Avatar Repo :: HomePage" ||
-  document.title == "Avatar Repo :: Clothing Original"
-) {
+if (document.title == "Avatar Repo :: HomePage") {
 } else {
   const mainSection = document.getElementById("main-section");
 
